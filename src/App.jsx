a69957327a4a69abf6aeffd1800196ba57ab9c29
ornob90 <oornob49@gmail.com>
2023-08-15 09:37:54 +0600

    first commit and nav section complete without respnosive

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import Navbar from "./components/Navbar";
+import viteLogo from "/vite.svg";
+
+function App() {
+  return <Navbar />;
+}
+
+export default App;
